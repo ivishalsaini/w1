3dfa1fdd77100fbe0358ed939dd166be01169fb6
Vishal Saini <vishalv1217@gmail.com>
2020-07-04 00:35:59 +0530

    Updates

---
+++ precache-manifest.7fdf640b9c1e722342bc884571284bdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc18b954b318e4913aa327cf3ffc3865",
+    "url": "/w1/index.html"
+  },
+  {
+    "revision": "0babba9483ffe185e842",
+    "url": "/w1/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "e62b30c6a39344fbc65d",
+    "url": "/w1/static/css/main.945c5560.chunk.css"
+  },
+  {
+    "revision": "0babba9483ffe185e842",
+    "url": "/w1/static/js/2.97a191ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/w1/static/js/2.97a191ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e62b30c6a39344fbc65d",
+    "url": "/w1/static/js/main.a33144c0.chunk.js"
+  },
+  {
+    "revision": "8fb65594ff8aa83e1b13",
+    "url": "/w1/static/js/runtime-main.f248d044.js"
+  }
+]);
